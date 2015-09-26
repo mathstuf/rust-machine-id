c5caf8b29305771220d6389719cf283536c0fcc8
Ben Boeckel <mathstuf@gmail.com>
2015-09-25 20:49:30 -0400

    unix: simplify error handling

--- src/imp/unix.rs
+++ src/imp/unix.rs
@@ -1,21 +1,19 @@
 extern crate uuid;
 use self::uuid::Uuid;
 
+use std::error::Error;
 use std::fs::File;
-use std::io;
 use std::io::{BufRead, BufReader};
 
-fn _get_machine_id() -> io::Result<Uuid> {
+fn _get_machine_id() -> Result<Uuid, Box<Error>> {
     let fin = try!(File::open("/etc/machine-id"));
     let mut reader = BufReader::new(fin);
     let mut line = String::new();
 
     try!(reader.read_line(&mut line));
+    line.truncate(32);
 
-    match Uuid::parse_str(&line) {
-        Ok(uuid) => Ok(uuid),
-        Err(_)   => Err(io::Error::last_os_error()),
-    }
+    Ok(try!(Uuid::parse_str(&line)))
 }
 
 pub fn get_machine_id() -> Option<Uuid> {
